4e7ca1b9f3da9ae67747cb68612b1b2f03d96bbb
Vaibhav4-creator <53190703+Vaibhav4-creator@users.noreply.github.com>
2021-03-14 16:13:03 +0530

    Add files via upload

---
+++ Quiz game/js/questions2.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "How many pseudo-classes are there?",
+        answer: "4",
+        options: [
+            "1",
+            "4",
+            "5",
+            "7"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How can you reduce page load time?",
+        answer: "All of the above",
+        options: [
+            "Reduce img size",
+            "Reduce lookups",
+            "Clean web code",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "CORS stands for?",
+        answer: "Cross Origin Resource Sharing",
+        options: [
+            "computer orient resource stand",
+            "Common Object Resource Share",
+            "Both 1 and 2",
+            "Cross Origin Resource Sharing"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
